186a7be1be1db1d0b1fcd80195a4eaebf4781ccd
ahmed <hamzaa667788@gmail.com>
2024-10-19 10:43:36 -0400

    changes to index

--- react_native_jobs/app/index.tsx
+++ react_native_jobs/app/index.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native'
+import { Stack, useRouter } from 'expo-router'
+import { COLORS, icons, SIZES, images } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 const Home = () => {
+    const router = useRouter();
     return (
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+            options={{
+                headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+            }}
+            />
+        </SafeAreaView>
         )
 }
 export default Home;
