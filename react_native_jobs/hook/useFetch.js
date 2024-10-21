5eb81212f21b73f5461c4536568da6bebd9aa625
ahmed <hamzaa667788@gmail.com>
2024-10-20 21:34:56 -0400

    useFetch

--- react_native_jobs/hook/useFetch.js
+++ react_native_jobs/hook/useFetch.js
@@ -2,5 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useFetch = () => {
-    
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
 }
