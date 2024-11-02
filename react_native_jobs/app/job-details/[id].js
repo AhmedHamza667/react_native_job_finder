34e160f62140932428d1137c0efb972dcc805405
ahmed <hamzaa667788@gmail.com>
2024-11-02 11:07:08 -0400

    job details header left

--- react_native_jobs/app/job-details/[id].js
+++ react_native_jobs/app/job-details/[id].js
@@ -1,20 +1,40 @@
 import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react-native'
 import { Stack, useRouter, useSearchParams } from 'expo-router'
 import { useCallback, useState } from 'react'
+import { useRoute } from '@react-navigation/native';
 
 import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components'
 import { COLORS, icons, SIZES } from '../../constants'
-import { useFetch } from '../../hook/useFetch'
+import useFetch from '../../hook/useFetch'
 
 function JobDetils() {
-    const params = useSearchParams();
     const router = useRouter();
-    const [ data, isLoading, error, refetch ] = useFetch('job-details', {
+    const route = useRoute();
+    const params = route.params;
+
+    const { data, isLoading, error, refetch } = useFetch(
+        'job-details', {
         job_id: params.id
-    });
+    })
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-        <Stack.Screen></Stack.Screen>
+        <Stack.Screen
+          options={{
+            headerStyle: { background: COLORS.lightWhite },
+            headerShadowVisible: false,
+            headerBackVisible: false,
+            headerLeft: () => (
+                <ScreenHeaderBtn 
+                iconUrl={icons.left}
+                dimension="60%"
+                handlePress={() => router.back()}
+                /> 
+            )
+
+          }}>
+            
+        </Stack.Screen>
     </SafeAreaView>
   )
 }
