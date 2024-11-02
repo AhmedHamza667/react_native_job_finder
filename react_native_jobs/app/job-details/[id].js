072e45c818275093b3e2a951fb02733f552da87d
ahmed <hamzaa667788@gmail.com>
2024-11-02 12:10:11 -0400

    company

--- react_native_jobs/app/job-details/[id].js
+++ react_native_jobs/app/job-details/[id].js
@@ -51,7 +51,11 @@ function JobDetils() {
             {isLoading ? <ActivityIndicator size={"large"} color={COLORS.primary}/> 
             : error? <Text>Something went wrong</Text> :data.length=== 0 ? (<Text>No data</Text>)
         : <View style={{padding: SIZES.medium, paddingBottom: 100}}>
-            <Company />
+            <Company 
+              companyLogo={data[0].employer_logo}
+              jobTitle={data[0].job_title}
+              companyName={data[0].employer_name}
+              location={data[0].job_country}/>
             <JobTabs />
         </View>
         }
